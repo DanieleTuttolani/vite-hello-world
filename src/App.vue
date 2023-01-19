4ae4bfb2fb0354baa2d62afed4cdfd64575717a6
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-19 15:29:50 +0100

    finito con bonus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue'
+export default{
+  data(){
+    return{
+
+    }
+  }
+  ,
+  components:{
+    AppTitle,
+    AppHero
+  }
+}
 </script>
 
 <template>
-  
+  <AppTitle></AppTitle>
+  <AppHero></AppHero>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+    <img src="https://w7.pngwing.com/pngs/802/825/png-transparent-redbubble-polite-cat-meme-funny-cat-meme-thumbnail.png" alt="">
+</template>
+
+<style></style>
